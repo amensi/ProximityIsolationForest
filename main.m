b885c08cef65c6107a4656c6968d398667eeafce
amensi <44211491+amensi@users.noreply.github.com>
2022-11-28 16:05:57 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -1,10 +1,5 @@
-%Example of how to build and test a Proximity Isolation Forest model
+%%% EXAMPLE OF HOW TO BUILD AND TEST A Proximity Isolation Forest MODEL %%%
 clear all;close all;clc
-
-%NOTE: The user hjas to download:
-% - prdisdata and distools from http://prtools.tudelft.nl/Guide/37Pages/distools.html 
-% - prtools from http://prtools.tudelft.nl/Guide/37Pages/software.html
-%Put the extracted folders in the current one (or modify the path below)
 
 %Number of iterations of the procedure
 nIt=10;
@@ -12,51 +7,32 @@ nIt=10;
 %Inlier and outlier class labels
 inl=0; outl=1;
 
-%Adding the paths
+%Adding the path
 addpath(genpath('./ProxIF_code/'));
-addpath(genpath('./prdisdata/')); addpath(genpath('./distools/')); addpath(genpath('./prtools/'));
 
 %Loading the dataset
-dataset_name='zongker';
-dataset=load(dataset_name);
-data=dataset.d; temp_labels=dataset.lab; N=size(data,1);
-%Zongker is a similarity matrix so we need to transform it into a
-%dissimilarity one.
-if strcmpi(dataset_name,'zongker')
-    data=dissimt(data,'SIM2DIS');
-end
+load('zongker_od.mat')
 
-%Labeling procedure: objects belonging to the class with the highest
-%scatter are labeled as outliers
-classes=unique(temp_labels);
-withinScatter=zeros(length(classes),1);
-for cl=1:length(classes)
-    temp=data(temp_labels==classes(cl),temp_labels==classes(cl));
-    withinScatter(cl)=mean(temp,'all');
-end
-[~,idx]=max(withinScatter);
-labels=zeros(N,1);
-labels(temp_labels~=classes(idx))=inl;
-labels(temp_labels==classes(idx))=outl;
-NOutl=sum(labels==outl); NInl=N-NOutl;
+%Finding the number and indexes of outliers and inliers in the dataset
+N=size(data,1); NOutl=sum(labels==outl); NInl=N-NOutl;
 inlIdx=find(labels==inl); outlIdx=find(labels==outl);
 
 %Setting the number and type of objects within the training partitions
-Ntr=ceil(N.*0.5); NtrO=ceil(Ntr.*0.05); %maximum number of outliers
-%If outliers are less than 5% of the training data, then we are going to use
+Ntr=ceil(N.*0.5);
+NtrO=ceil(Ntr.*0.05); %maximum number of outliers
+%If outliers are less than 5% of the training data, we are going to use
 %half for training and half for testing
 if NtrO>=NOutl
     NtrO=ceil(NOutl./2);
 end
 NtrI=Ntr-NtrO;
 
-%Setting the forest parameters
-%You can also comment Lines 57-60: parameters will be set in the
+%Setting the forest parameters.
+%You can also not set the fields of param, parameters will be set in the
 %training function according to a fixed parametrization.
-%Just remove param when calling ProxIF_wrap or  ProxIF_training
 param.max_comp=20;
 param.T=500;
-param.D='log'; param.crit='O-2PH'; 
+param.D='log'; param.crit='O-2PH';
 param.S=min(128,Ntr);
 
 auc=zeros(nIt,1);
@@ -75,7 +51,7 @@ for it=1:nIt
     %Training and testing procedure
     score=ProxIF_wrap(train,test,param);
 
-    % The commented lines below are an alternative to Line 27
+    %Alternative to the usage of the ProxIF_wrap function:
 %     forest=ProxIF_training(train,param);
 %     c_l=zeros(forest.param.S,1);
 %     for i=1:forest.param.S
@@ -86,4 +62,4 @@ for it=1:nIt
     [~,~,~,auc(it)]=perfcurve(labels_test,score,outl);
 end
 
-fprintf("\nZongker AUC Table 6 0.8496 VS found AUC %5.4f", mean(auc));
+fprintf("\nZongker AUC ProxIF-F Table 6 0.8496 VS found AUC %5.4f", mean(auc));
